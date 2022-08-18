8918c9d41767784ac13a1a7dee0912a2642ac509
Dastan <95084961+Sino0on@users.noreply.github.com>
2022-08-18 19:32:30 +0600

    Delete passenger_wsgi.py

--- passenger_wsgi.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import os, sys
-sys.path.insert(0, '/var/www/u1750434/data/www/zetroom.com/zetroom')
-sys.path.insert(1, '/var/www/u1750434/data/djangoenv/lib/python3.7/site-packages')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'zetroom.settings'
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
